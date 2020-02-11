8e3f7b333b000c5fd48dd9ea5888168e45ac81f0
Dani Santos <santos.adaniele@gmail.com>
2020-02-11 15:29:23 -0500

    first attempt at solving 6

--- workshop/1-objects/exercise-6-hard.js
+++ workshop/1-objects/exercise-6-hard.js
@@ -2,21 +2,21 @@
 // -------------------
 
 var favoriteDessert = {
-    scott: 'brownies',
-    fred: 'tiramisu',
-    lisa: 'chocolate cake',
-    riley: 'ice-cream',
-    sunny: 'cheese cake',
-    john: 'ice-cream',
-    beth: 'cheese cake',
-    summer: 'ice-cream',
-    morty: 'apple pie',
-    rick: 'brownies',
-    andrew: 'cheese cake',
-    jerry: 'rhubard pie',
-    'jean-luc': 'cheese cake',  
-    tiffany: 'waffles',
-    melissa: 'profiteroles',
+  scott: "brownies",
+  fred: "tiramisu",
+  lisa: "chocolate cake",
+  riley: "ice-cream",
+  sunny: "cheese cake",
+  john: "ice-cream",
+  beth: "cheese cake",
+  summer: "ice-cream",
+  morty: "apple pie",
+  rick: "brownies",
+  andrew: "cheese cake",
+  jerry: "rhubard pie",
+  "jean-luc": "cheese cake",
+  tiffany: "waffles",
+  melissa: "profiteroles"
 };
 
 // Above is an object with the results of a poll.
@@ -27,11 +27,28 @@ var favoriteDessert = {
 // e.g. 1. <DESSERT_NAME>
 //      2. <DESSERT_NAME>
 //      ...
+const desserts = new Set(Object.values(favoriteDessert));
 
+const createScore = uniqueDesserts => {
+  let dessertScores = {};
+  for (let key in favoriteDessert) {
+    desserts.forEach(value => {
+      if (value === favoriteDessert[key]) {
+        if (!dessertScores[value]) {
+          dessertScores[value] = 1;
+        } else {
+          dessertScores[value] += 1;
+        }
+      }
+    });
+  }
+  return dessertScores;
+};
 
-// B)
+console.log(createScore(desserts));
+// B
 // The names of those that said the same desserts. Output the list in
 // order by dessert.
 // e.g. - brownies: <NAME>, <NAME>, ...
 //      - ice-cream: <NAME>, <NAME>, <NAME>, ...
-//      ...
+//      ..
